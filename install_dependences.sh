b033267f411a2157e7d55bfc602c1e0cc16a5625
Elmapul <igccpao@gmail.com>
2016-06-08 06:43:01 -0300

    Update install_dependences.sh

--- install_dependences.sh
+++ install_dependences.sh
@@ -3,3 +3,6 @@
 #dependences for clipboard download
 sudo apt-get install xclip -y
 sudo apt-get install youtube-dl -y
+
+#Setup audio
+#http://www.overclock.net/a/how-to-get-the-best-sound-with-and-properly-configure-pulseaudio
