611725411b744534ebb07b1916c09767bf5bf0ce
Elmapul <igccpao@gmail.com>
2016-09-10 01:33:22 -0300

    Update install_dependences.sh

--- install_dependences.sh
+++ install_dependences.sh
@@ -21,3 +21,6 @@ sudo apt-get install npm
 #firefox? google-chrome?
 
 sudo apt-get install nodejs-legacy
+
+#caso não funcione
+#http://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
