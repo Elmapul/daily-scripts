a09a57e19d510242ab030a82a9f7778bffd091e2
Elmapul <igccpao@gmail.com>
2016-09-13 16:04:48 -0300

    Update charNameReplacer.js

--- charNameReplacer.js
+++ charNameReplacer.js
@@ -1,8 +1,26 @@
+/*how to use
+*use your favorite text editor to write your story, and bold every character that apear on it
+*save as .html
+*add this script, and follow the instructions bellow:
+*list the name of the characters of your history in an array.
+*example:
+*personagens=["fluffy","John"];
+*call the function passing the array as parameter
+*example
+*characterReplace(personagens);
 
-personagens=["fluffy","John"];
+*opitional, create an css to show character name and info and aply it to <character> tag
+*/
+
+
+
+function characterReplace(per){
+this.personagens=per;
 for (i=0;i<personagens.length;i++){
 var personagem=personagens[i];
 $( "b:contains('"+personagem+"')" ).each(function() {
   $( this ).replaceWith( "<personagem>" + $( this ).text() + "</personagem>" );
 });
+}
+
 }
