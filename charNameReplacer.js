8cc18aee3a2e42df9f47a0dca17f9110cb601f21
Elmapul <igccpao@gmail.com>
2016-09-13 15:54:47 -0300

    Update charNameReplacer.js

--- charNameReplacer.js
+++ charNameReplacer.js
@@ -1,6 +1,8 @@
 
-
-$( "b:contains('John')" ).each(function() {
+personagens=["fluffy","John"];
+for (i=0;i<personagens.length;i++){
+var personagem=personagens[i];
+$( "b:contains('"+personagem+"')" ).each(function() {
   $( this ).replaceWith( "<personagem>" + $( this ).text() + "</personagem>" );
 });
-
+}
