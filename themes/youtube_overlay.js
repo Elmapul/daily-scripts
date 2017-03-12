88bf793ef8d6690daa675d4e32235355df49b25d
Elmapul <igccpao@gmail.com>
2017-03-12 05:03:43 -0300

    Update youtube_overlay.js

--- themes/youtube_overlay.js
+++ themes/youtube_overlay.js
@@ -7,15 +7,14 @@ img.style.position="relative";
 img.style.zIndex = "1000";
 
 //window mode
-function acordar(){
+
 var src = document.getElementsByClassName("player-api");
 var aux0=src.item(0);
 var aux1=aux0.offsetHeight;
 var tmp="-"+aux1+"px";
 img.style.top=tmp;
 src[0].appendChild(img);
-  }
-acordar();
+
   
 //Full screen
 //video-stream html5-main-video
